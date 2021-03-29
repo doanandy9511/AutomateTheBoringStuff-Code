d53dd33e3759c73f530ae0f112b9bef90f347704
doanandy9511 <doanandy9511@yahoo.com>
2021-03-29 00:29:15 -0700

    enumerate for loop

--- Chapter 4 - Lists/myPets.py
+++ Chapter 4 - Lists/myPets.py
@@ -7,3 +7,8 @@ if name not in myPets:
     print('I do not have a pet named {}'.format(name))
 else:
     print('{} is my pet.'.format(name))
+
+# Enuerate practice
+supplies = ['pen','stapler','flamethrower','binders']
+for i,item in enumerate(supplies):
+    print('Index {0} in supplies is: {1}'.format(i,item))
