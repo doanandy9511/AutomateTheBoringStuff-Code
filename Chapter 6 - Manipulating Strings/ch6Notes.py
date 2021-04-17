656adae7338a6fae1fd9262b709f8c71c33e7d7c
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2021-04-16 22:53:41 -0700

    ch 6 notes

---
+++ Chapter 6 - Manipulating Strings/ch6Notes.py
@@ -0,0 +1,87 @@
+# %% String Literals, Double Quotes
+spam = "That is Alice's cat."
+spam
+
+# %% Escape Characters
+# an escape character lets you use characters that are 
+#  otherwise impossible to put into a string
+spam = 'Say hi to Bob\'s mother.'
+spam
+#           \'            \"   \t       \n         \\
+# single quote, double quote, tab, newline, backslash
+# %% Raw Strings
+# you can place an r before the beginning quotation mark
+#  of a string to make it a raw string
+print(r'That is Carol\'s cat.')
+# raw strings are helpful when typing string values
+#  that contain many backslashes, such as file paths
+# %% Multiline Strings with Triple Quotes
+# catnapping.py
+'''Multiline Comments
+the # character marks the beginning of a comment for
+the rest of the line, but multiline string is often
+used for comments that span multiple lines'''
+
+# %% Indexing and Slicing Strings
+spam = 'Hello, world!'
+spam[0] # 'H'
+spam[4] # 'o'
+spam[-1] # '!'
+spam[0:5] # 'Hello'
+spam[:5] # 'Hello'
+spam[7:] # 'world!'
+# %% in and not in operators w/ strings
+# 'Hello' in 'Hello' : True
+# '' in 'spam' : True
+# 'cats' not in 'cats and dogs' : False
+
+# %% Putting strings inside other strings
+name = 'Andy'
+age = 420
+f'My name is {name}. Next year I will be {age + 1}.'
+# 'My name is Andy. Next year I will be 421.'
+# %% Useful string methods
+# upper(), lower(), isupper(), islower() methods
+spam = 'Hello, world!'
+spam = spam.upper()
+print(spam) # HELLO, WORLD!
+spam = spam.lower()
+print(spam) # hello, world!
+
+# %%
+feeling = input('How are you?\n')
+if feeling.lower() == 'great':
+    print('I feel great too.')
+else:
+    print('I hope the rest of your day is good.')
+# %%
+spam = 'Hello, world!'
+spam.islower()
+# False
+# %%
+spam.isupper()
+# False
+# %%
+'HELLO'.isupper()
+# True
+# %%
+'abc12345'.islower()
+# True
+# %%
+'12345'.islower()
+# False
+# %%
+'12345'.isupper()
+# False
+# %%
+'Hello'.upper()
+# 'HELLO'
+'Hello'.upper().lower()
+# 'hello'
+'Hello'.upper().lower().upper()
+# 'HELLO'
+'HELLO'.lower()
+# 'hello'
+'HELLO'.lower().islower()
+# True
+# %% the isX() methods
