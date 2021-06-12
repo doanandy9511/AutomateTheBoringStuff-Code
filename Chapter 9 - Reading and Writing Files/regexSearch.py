376d3460ccad2e39bea66407eeb7bd26dc8aa0cb
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2021-06-11 17:34:02 -0700

    finish ch9

--- Chapter 9 - Reading and Writing Files/regexSearch.py
+++ Chapter 9 - Reading and Writing Files/regexSearch.py
@@ -4,10 +4,15 @@ from pathlib import Path
 def main():
     if len(sys.argv) > 2:
         directory = Path(sys.argv[1])
-        if directory.is_dir():
-            print('woohoo')
-            print(list(directory.glob('*.txt')))
         regex = sys.argv[2]
+        searchRe = re.compile(regex)
+        if directory.is_dir():
+            for file in list(directory.glob('*.txt')):
+                with open(file) as file:
+                    lines = file.readlines()
+                    for line in lines:
+                        if searchRe.search(line) != None:
+                            print(line.strip())
     else:
         print('BOOO, not enough parameters.')
     
