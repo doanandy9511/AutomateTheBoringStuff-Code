a3a21853d6594ca397ead4445accbe1fe4ceefd5
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2021-06-21 23:24:00 -0700

    finish ch10

--- Chapter 10 - Organizing Files/deleteUnneededFiles.py
+++ Chapter 10 - Organizing Files/deleteUnneededFiles.py
@@ -1,8 +1,23 @@
 # deleteUnneededFiles.py - walks through folder tree of hard drive
 #  and searches for 
+import os
+from pathlib import Path
+
+def scanDrive(drive):
+    for foldername, subfolders, filenames in os.walk(drive):
+        for filename in filenames:
+            filename = Path(foldername) / filename
+            size = os.path.getsize(filename)
+            sizeMB = size // (2**20)
+            sizeGB = sizeMB / 1024
+
+            if sizeGB >= 1:
+                print(f'{foldername}\\{filename} : {sizeGB:.2f} GB')
+            elif sizeMB > 100:
+                print(f'{foldername}\\{filename} : {sizeMB} MB')
 
 def main():
-    pass
+    scanDrive('C:\\')
 
 if __name__ == '__main__':
     main()
