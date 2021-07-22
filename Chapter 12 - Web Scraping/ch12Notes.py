1a1cc0630b53500cd5efe4dc4627fcd29d08a022
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2021-07-21 21:42:42 -0700

    start ch12

---
+++ Chapter 12 - Web Scraping/ch12Notes.py
@@ -0,0 +1,10 @@
+# Chapter 12 - Web Scraping notes
+
+#%% Web scraping is the term for using a program to download and process content from the web
+#webbrowser Comes with Python and opens a browser to a specific page.
+#requests Downloads files and web pages from the internet.
+#bs4 Parses HTML, the format that web pages are written in.
+#selenium Launches and controls a web browser. The selenium module is able to fill in forms and simulate mouse clicks in this browser.
+import webbrowser
+webbrowser.open('https://inventwithpython.com')
+# %%
