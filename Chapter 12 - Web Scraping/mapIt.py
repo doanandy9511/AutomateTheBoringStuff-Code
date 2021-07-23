ce3fe9a3182d7288b5651b12a8d422b6f0d52495
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2021-07-23 00:12:32 -0700

    mapIt project

---
+++ Chapter 12 - Web Scraping/mapIt.py
@@ -0,0 +1,15 @@
+# mapIt.py - launches a map in the browser using an 
+# address from the command line or clipboard
+import webbrowser, sys, pyperclip
+
+def main():
+    addr = ''
+    if len(sys.argv) > 1:
+        # get addr from cmd line
+        addr = ' '.join(sys.argv[1:])
+    else:
+        addr = pyperclip.paste()
+    
+    webbrowser.open(f'https://www.google.com/maps/place/{addr}')
+if __name__ == '__main__':
+    main()
