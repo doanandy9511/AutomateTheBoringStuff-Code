f45afd5ff7311877a566c8fb4a4d60a1a38e2a41
doanandy9511 <61335279+doanandy9511@users.noreply.github.com>
2022-06-18 00:51:49 -0700

    Update ch12Notes.py
    Update notes

--- Chapter 12 - Web Scraping/ch12Notes.py
+++ Chapter 12 - Web Scraping/ch12Notes.py
@@ -100,4 +100,38 @@ exampleFile = open('example.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile, 'html.parser')
 type(exampleSoup)
 # bs4.BeautifulSoup
+# %% Finding an Element with the select() method
+# You can retrieve a web page element by calling select() and passing
+# a string of a CSS selector for the element you are looking for
+# soup.select('div')        -   All elements named <div>
+# soup.select('#author')    -   The element with an id attribute of author
+# soup.select('.notice')    -   All elements that use a CSS class attribute
+#                               named notice
+# soup.select('div span')   -   All elements named <span> that are within
+#                               an element named <div>
+# soup.select('div > span') -   All elements named <span> that are directly
+#                               within an element named <div>, with no 
+#                               other element in between
+# soup.select('input[name]')-   All elements named <input> that have a name 
+#                               attribute with any value
+# soup.select('input[type="button"]')
+#                           -   All elements named <input> that have an 
+#                               attribute named type with value button
+import bs4
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile.read(), 'html.parser')
+elems = exampleSoup.select('#author')
+type(elems)
+# bs4.element.ResultSet
+len(elems)
+# 1 
+type(elems[0])
+# bs4.element.Tag
+str(elems[0])
+# '<span id="author">Al Sweigart</span>'
+elems[0].getText()
+# 'Al Sweigart'
+elems[0].attrs
+# {'id': 'author'}
+
 # %%
